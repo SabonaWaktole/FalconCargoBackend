e293f5344e5268d25e07d3a32457da965a116121
SabonaWaktole <sebonawaktole@gmail.com>
2025-09-17 23:37:01 +0300

    create person model

--- src/main/java/com/nordic/cargo/backend/Model/PersonModel.java
+++ src/main/java/com/nordic/cargo/backend/Model/PersonModel.java
@@ -1,54 +1,54 @@
 package com.nordic.cargo.backend.Model;
 
 
-//import jakarta.persistence.*;
-//import lombok.*;
-//import jakarta.validation.constraints.NotNull;
-//
-//import java.util.List;
+import jakarta.persistence.*;
+import lombok.*;
+import jakarta.validation.constraints.NotNull;
 
-//@Entity
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Builder
-//@Data
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
 public class PersonModel {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Integer id;
-//
-//    @Column(unique = true, nullable = false)
-//    private String email;
-//
-//    private  String firstName;
-//
-//    private String middleName;
-//
-//    private String lastName;
-//
-//    @NotNull
-//    private String primaryAddress;
-//
-//    @NotNull
-//    private String city;
-//
-//    private String state;
-//
-//    @NotNull
-//    private String country;
-//
-//    @NotNull
-//    private String zipCode;
-//
-//    @NotNull
-//    private String phoneNumber;
-//
-//    private  String faxNumber;
-//
-//    private String secondaryAddress;
-//
-//    private List<String> languages;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
-//    private String vat;
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private  String firstName;
+
+    private String middleName;
+
+    private String lastName;
+
+    @NotNull
+    private String primaryAddress;
+
+    @NotNull
+    private String city;
+
+    private String state;
+
+    @NotNull
+    private String country;
+
+    @NotNull
+    private String zipCode;
+
+    @NotNull
+    private String phoneNumber;
+
+    private  String faxNumber;
+
+    private String secondaryAddress;
+
+    private List<String> languages;
+
+    private String vat;
 
 }
