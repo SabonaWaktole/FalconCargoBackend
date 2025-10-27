693b95d66a06692039824be7096ef82f84f1d637
SabonaWaktole <sebonawaktole@gmail.com>
2025-10-27 19:02:47 +0300

    final version 1

--- src/main/java/com/nordic/cargo/backend/Controller/GoodController.java
+++ src/main/java/com/nordic/cargo/backend/Controller/GoodController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 @RestController
 @RequestMapping("api/v1/goods")
+@CrossOrigin(origins = "http://localhost:5173")
 public class GoodController {
     private final GoodService goodService;
 
